beb7e2bf5b516437bc911062161a85ae36379010
xing <xing.fu.wur@gmail.com>
2021-01-01 12:15:50 +0100

    no message

--- mysite/article/urls.py
+++ mysite/article/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from . import views, api_views
+from . import views
 
 urlpatterns = [
     path('<int:article_id>',views.blog_detail, name='blog_detail'),
-    path('v1/post',api_views.api_post, name='update'),
-    path('v1/test',api_views.test, name='test'),
+
 
 ]
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'sdfsadfasdfsafsd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,19 +76,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'slave': {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
 
-    'default':{
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
 }
 
 
